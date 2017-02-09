f10d693ad0839b474ad0c482950e40a19404738e
gerry candy <g3candy@gmail.com>
2017-02-09 18:53:22 +0000

    added imbue test

---
+++ test/test_imbue.cpp
@@ -0,0 +1,23 @@
+#include "dsl_compiler.h"
+#include "placeholders.h"
+
+#include "test_ctx_fixture.h"
+
+#include <gtest/gtest.h>
+
+namespace{
+        using namespace dsl_compiler;
+        using namespace dsl_compiler::placeholders;
+
+        using ImbueTest = BasicCtxFixture<struct tag_test_dsl>;
+
+        TEST_F( ImbueTest, simple){
+                ( if_(false_)[
+                        _1 = 1
+                  ].else_[
+                        _1 =2
+                  ] )();
+        }
+
+
+}
