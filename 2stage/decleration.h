8b81c0b48fd3e925f7808bc823b37e9d59635c87
gerry candy <g3candy@gmail.com>
2017-02-12 11:50:22 +0000

    added static type checking to function

--- 2stage/decleration.h
+++ 2stage/decleration.h
@@ -4,17 +4,14 @@
 
 namespace dsl_compiler{
 
-
         template<class Return_Type>
         struct function{
                 explicit function(statement stmt):stmt_{std::move(stmt)}{}
-
-                template<class... Args>
+                template<class... Args, class = void_t< decltype(funamental_t{std::declval<Args>()})...> >
                 Return_Type operator()(Args&&... args){
                         auto tmp{ stmt_(std::forward<Args>(args)...)};
                         return static_cast<Return_Type>(tmp);
                 }
-                statement_impl_t to_stmt_impl_t_()const{ return stmt_.to_stmt_impl_t_(); }
         private:
                 statement stmt_;
         };
